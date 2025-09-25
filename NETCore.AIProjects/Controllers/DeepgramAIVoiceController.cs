4d79582fbf4213c9fb5bf4c416d115bb4b9d14c6
tunadeveloper <tunabusiness25@gmail.com>
2025-09-25 22:04:54 +0300

    Updated

--- NETCore.AIProjects/Controllers/DeepgramAIVoiceController.cs
+++ NETCore.AIProjects/Controllers/DeepgramAIVoiceController.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 using System.Net.Http.Headers;
 
 namespace NETCore.AIProjects.Controllers
-{ 
+{
     public class DeepgramAIVoiceController : Controller
     {
         private readonly string apiKey = "API_KEY";
@@ -17,37 +17,40 @@ namespace NETCore.AIProjects.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(IFormFile audioFile)
         {
-            if(audioFile == null || audioFile.Length == 0)
+            if (audioFile == null || audioFile.Length == 0)
             {
-                ViewBag.Transcript = "Lütfen Bir ses dosyası seçiniz.";
+                ViewBag.Transcript = "Lütfen bir ses dosyası seçiniz.";
                 return View("Index");
             }
 
-            using(var client = new HttpClient())
+            using (var client = new HttpClient())
             {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", apiKey);
-                using(var content = new MultipartFormDataContent())
+                client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Token", apiKey);
+
+                using (var stream = audioFile.OpenReadStream())
+                using (var content = new StreamContent(stream))
                 {
-                    var streamContent = new StreamContent(audioFile.OpenReadStream());
-                    streamContent.Headers.ContentType = new MediaTypeHeaderValue("audio/mpeg");
-                    content.Add(streamContent, "audio", audioFile.FileName);
+                    content.Headers.ContentType = new MediaTypeHeaderValue(audioFile.ContentType);
 
                     var url = "https://api.deepgram.com/v1/listen?language=tr";
                     var response = await client.PostAsync(url, content);
+
+                    var json = await response.Content.ReadAsStringAsync();
+
                     if (!response.IsSuccessStatusCode)
                     {
-                        var errorMsg = await response.Content.ReadAsStringAsync();
-                        ViewBag.Transcript = "Transkripsiyon başarısız oldu! Hata: " + errorMsg;
+                        ViewBag.Transcript = "Transkripsiyon başarısız oldu! Hata: " + json;
                         return View("Index");
                     }
 
-                    var json = await response.Content.ReadAsStringAsync();
                     var result = JsonConvert.DeserializeObject<dynamic>(json);
                     var transcript = result?.results?.channels[0]?.alternatives[0]?.transcript;
-                    ViewBag.Transcript = transcript ?? "Transkript Boş";
+                    ViewBag.Transcript = transcript ?? "Transkript boş";
                 }
-        }
+            }
+
             return View("Index");
         }
-}
+    }
 }
