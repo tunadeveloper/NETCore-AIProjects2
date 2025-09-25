a403ce091b10b389f64244153d76cc3d7fdad9cb
tunadeveloper <tunabusiness25@gmail.com>
2025-09-25 21:49:39 +0300

    Add project files.

---
+++ NETCore.AIProjects/Controllers/DeepgramAIVoiceController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace NETCore.AIProjects.Controllers
+{ 
+    public class DeepgramAIVoiceController : Controller
+    {
+        private readonly string apiKey = "API_KEY";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile audioFile)
+        {
+            if(audioFile == null || audioFile.Length == 0)
+            {
+                ViewBag.Transcript = "Lütfen Bir ses dosyası seçiniz.";
+                return View("Index");
+            }
+
+            using(var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", apiKey);
+                using(var content = new MultipartFormDataContent())
+                {
+                    var streamContent = new StreamContent(audioFile.OpenReadStream());
+                    streamContent.Headers.ContentType = new MediaTypeHeaderValue("audio/mpeg");
+                    content.Add(streamContent, "audio", audioFile.FileName);
+
+                    var url = "https://api.deepgram.com/v1/listen?language=tr";
+                    var response = await client.PostAsync(url, content);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        var errorMsg = await response.Content.ReadAsStringAsync();
+                        ViewBag.Transcript = "Transkripsiyon başarısız oldu! Hata: " + errorMsg;
+                        return View("Index");
+                    }
+
+                    var json = await response.Content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<dynamic>(json);
+                    var transcript = result?.results?.channels[0]?.alternatives[0]?.transcript;
+                    ViewBag.Transcript = transcript ?? "Transkript Boş";
+                }
+        }
+            return View("Index");
+        }
+}
+}
---
+++ NETCore.AIProjects/wwwroot/Soft-UI-Dashboard-Tailwind-1.0.0/README.md
@@ -0,0 +1,42 @@
+# Soft-UI-Dashboard-Tailwind
+
+## 🚀 Preview
+
+- [Live Demo](https://themewagon.github.io/Soft-UI-Dashboard-Tailwind/)
+
+## 📥 Download
+
+- [Download from Creative Tim](https://www.creative-tim.com/product/soft-ui-dashboard-tailwind?ref=readme-sudt)
+
+## 🛠 Getting Started
+
+1. **Clone the repository**
+
+   ```bash
+   git clone https://github.com/themewagon/Soft-UI-Dashboard-Tailwind
+   ```
+
+2. Install Dependencies
+
+```
+
+npm i
+
+```
+
+3. View the site locally
+
+## Author
+
+Design and UI concepts are crafted by the [Creative Tim](https://www.creative-tim.com/) team, based on their Soft UI Dashboard Tailwind product.
+This template adapts their work for an educational website.
+
+## License
+
+- Design and code © [Creative Tim](https://www.creative-tim.com/)
+- Licensed under the [MIT License](https://github.com/creativetimofficial/soft-ui-dashboard-tailwind/blob/main/LICENSE.md)
+- Distributed by [ThemeWagon](https://themewagon.com)
+
+```
+
+```
